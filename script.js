db10feff85c58659680bb219a9768427b69e1f82
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-03 20:30:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
-    //Write your code here
+    let form = document.getElementById('form1'); // replace 'yourFormId' with the id of your form
+    let firstName = form.elements['fname'].value; // replace 'firstName' with the name of your input field for the first name
+    let lastName = form.elements['lname'].value; // replace 'lastName' with the name of your input field for the last name
 
+    // Now you can use the firstName and lastName variables
 }
